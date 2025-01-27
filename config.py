f11d0f778f37d9236fd87bd2443562f2824433d5
Anton Elesin <31272958+elr1c180@users.noreply.github.com>
2025-01-27 20:21:21 +0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import django 
-import os, sys
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chim_admin.settings')
-django.setup()
